ee37cbb062a8c0ef304963f4d1d5d9e5c5cc6f58
Calum Gilchrist <calum@homelinen.org>
2012-04-21 08:30:31 +0100

    Added a Simple Body to Planetoid

--- core/src/main/java/com/calumgilchrist/ld23/tinyworld/core/Planetoid.java
+++ core/src/main/java/com/calumgilchrist/ld23/tinyworld/core/Planetoid.java
@@ -1,16 +1,22 @@
 package com.calumgilchrist.ld23.tinyworld.core;
 
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.World;
+
 public class Planetoid {
 	private Sprite sprite;
+	private Body pBody;
 	
 	private int x;
 	private int y;
 	
-	public Planetoid(int posX, int posY, Sprite s){
+	public Planetoid(int posX, int posY, Sprite s, BodyDef bodyDef, World world){
 		x = posX;
 		y = posY;
 		
 		sprite = s;
+		pBody = world.createBody(bodyDef);
 	}
 	
 	public Sprite getSprite(){
