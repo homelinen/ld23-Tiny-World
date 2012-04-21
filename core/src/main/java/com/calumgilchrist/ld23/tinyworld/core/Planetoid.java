a7ef4dc096035ce20d656e646fc65435e6b8467d
Dan Bell <dbell.4.92@gmail.com>
2012-04-21 07:46:48 +0100

    Added x and y coords to Planetoid.java

--- core/src/main/java/com/calumgilchrist/ld23/tinyworld/core/Planetoid.java
+++ core/src/main/java/com/calumgilchrist/ld23/tinyworld/core/Planetoid.java
@@ -1,7 +1,24 @@
 package com.calumgilchrist.ld23.tinyworld.core;
 
-import playn.core.GroupLayer;
-
 public class Planetoid {
 	private Sprite sprite;
+	
+	private int x;
+	private int y;
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
 }
