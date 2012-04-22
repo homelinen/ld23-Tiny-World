d324356a06b31790d026c29ad924ab45196573c9
Dan Bell <dbell.4.92@gmail.com>
2012-04-22 17:14:57 +0100

    Created Comet.java, identical to Asteroid.java for now

---
+++ core/src/main/java/com/calumgilchrist/ld23/tinyworld/core/Comet.java
@@ -0,0 +1,13 @@
+package com.calumgilchrist.ld23.tinyworld.core;
+
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.World;
+
+public class Comet extends Planetoid{
+
+	public Comet(Sprite s, BodyDef bodyDef, World world) {
+		super(s, bodyDef, world);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
