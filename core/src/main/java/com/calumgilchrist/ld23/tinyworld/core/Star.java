d8963629cfd88f95a654606e8dccae35ec85b857
Dan Bell <dbell.4.92@gmail.com>
2012-04-22 17:16:06 +0100

    Created Star.java, identical to Asteroid.java

---
+++ core/src/main/java/com/calumgilchrist/ld23/tinyworld/core/Star.java
@@ -0,0 +1,13 @@
+package com.calumgilchrist.ld23.tinyworld.core;
+
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.World;
+
+public class Star extends Planetoid{
+
+	public Star(Sprite s, BodyDef bodyDef, World world) {
+		super(s, bodyDef, world);
+		// TODO Auto-generated constructor stub
+	}
+
+}
